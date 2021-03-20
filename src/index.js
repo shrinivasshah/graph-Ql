7d5856793fa1ffd2be02327e4afb8a8a6c1260b2
shrinivasshah <shrinivasshah05@gmail.com>
2021-03-20 07:42:52 +0530

    added subscription

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import { GraphQLServer } from "graphql-yoga";
-import uuidv4 from "uuid/v4";
+import { GraphQLServer, PubSub } from "graphql-yoga";
 import db from "./db";
 import Query from "./resolvers/Query";
 import Mutation from "./resolvers/Mutation";
+import Subscription from "./resolvers/Subscription";
 import Comment from "./resolvers/Comment";
 import Post from "./resolvers/Post";
 import User from "./resolvers/User";
@@ -12,17 +12,21 @@ import User from "./resolvers/User";
 
 // Resolvers
 
+const pubsub = new PubSub();
+
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
   resolvers: {
     Query,
     Mutation,
+    Subscription,
     User,
     Post,
     Comment,
   },
   context: {
     db,
+    pubsub,
   },
 });
 
--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,5 @@
+import uuidv4 from "uuid/v4";
+
 const Mutation = {
   // create user
   createUser(parent, args, { db: { users } }, info) {
@@ -66,7 +68,7 @@ const Mutation = {
   },
 
   // create post
-  createPost(parent, args, { db: { users, posts } }, info) {
+  createPost(parent, args, { db: { users, posts }, pubsub }, info) {
     const userExists = users.some((user) => user.id === args.data.author);
 
     if (!userExists) {
@@ -79,27 +81,45 @@ const Mutation = {
     };
 
     posts.push(post);
-
+    if (args.data.published) {
+      pubsub.publish("post", {
+        post: {
+          mutation: "CREATED",
+          data: post,
+        },
+      });
+    }
     return post;
   },
 
   // delete post
-  deletePost(parent, args, { db: { posts, comments } }, info) {
+  deletePost(parent, args, { db: { posts, comments }, pubsub }, info) {
     const postIndex = posts.findIndex((post) => post.id === args.id);
 
     if (postIndex === -1) {
       throw new Error("Post not found.");
     }
 
-    const deletedPosts = posts.splice(postIndex, 1);
+    const [post] = posts.splice(postIndex, 1);
+
     comments = comments.filter((comment) => comment.post !== args.id);
-    return deletedPosts[0];
+    if (post.published) {
+      pubsub.publish("post", {
+        post: {
+          mutation: "DELETED",
+          data: post,
+        },
+      });
+    }
+
+    return post;
   },
 
   // update post
-  updatePost(parents, args, { db: { posts } }, info) {
+  updatePost(parents, args, { db: { posts }, pubsub }, info) {
     const { id, data } = args;
     const post = posts.find((post) => post.id === id);
+    const originalPost = { ...post };
     if (!post) {
       throw new Error("No post found with that ID");
     }
@@ -111,12 +131,42 @@ const Mutation = {
     }
     if (typeof data.published === "boolean") {
       post.published = data.published;
+      if (originalPost.published && !post.published) {
+        // deleted
+        pubsub.publish("post", {
+          post: {
+            mutation: "DELETED",
+            data: originalPost,
+          },
+        });
+      } else if (!originalPost.published && post.published) {
+        // created
+        pubsub.publish("post", {
+          post: {
+            mutation: "CREATED",
+            data: post,
+          },
+        });
+      } else if (post.published) {
+        // updated
+        pubsub.publish("post", {
+          post: {
+            mutation: "UPDATED",
+            data: post,
+          },
+        });
+      }
     }
     return post;
   },
 
   // create comment
-  createComment(parent, args, { db: { users, posts, comments } }, info) {
+  createComment(
+    parent,
+    args,
+    { db: { users, posts, comments }, pubsub },
+    info
+  ) {
     const userExist = users.some((user) => user.id === args.data.author);
     const postExist = posts.some(
       (post) => post.id === args.data.post && post.published
@@ -130,6 +180,7 @@ const Mutation = {
       ...args.data,
     };
     comments.push(comment);
+    pubsub.publish(`comment ${args.data.post}`, { comment });
     return comment;
   },
   // delete comment
